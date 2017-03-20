028dbbc7b8f761f2d35bf9f7be05862950a2a69d
boweiYan <yanbowei@gmail.com>
2017-03-20 17:46:09 -0500

    code updated
    RSC.m kmeans fixed

--- files/robustness_code/rsc.m
+++ files/robustness_code/rsc.m
@@ -1,6 +1,5 @@
-
-function [class,U1] = rsc(A, K, method, prior)
-
+function [class,U1] = rsc(A, K, method)
+%
 % Regularized spectral clustering
 % Input: A: Adjacency matrix
 %        K: number of clusters
@@ -13,36 +12,16 @@ function [class,U1] = rsc(A, K, method, prior)
 %Author: Bowei Yan
 %Last Update: Mar 20, 2017
 
-    if nargin==3,
-        prior = 1/K*ones(K,1);
-    end
     [nv,~] = size(A);
     tau = mean(A(:));
     if strcmp(method,'pos')
         A_tau = A + tau*ones(nv,nv);
         L_tau = normalizeSym(A_tau);
-    elseif strcmp(method,'neg')
-        L = normalizeSym(A);
-        d = A*ones(nv,1);
-        d = 1/sqrt(d);
-        rho = .02; p = 0.8*rho;q = 0.2*rho;
-        alpha = log(p*(1-q)/q/(1-p));
-        beta = log((1-p)/(1-q));
-        L_tau = alpha*L + beta*d*d';
-        [~,l,~]=svd(L_tau);
-        l=diag(l);
     elseif strcmp(method,'noreg')
         L_tau = normalizeSym(A);
     elseif strcmp(method,'adj')      
     % Use adjacency matrix
         L_tau = (A+A')/2;
-    elseif strcmp(method,'deg')
-        % L = normalizeSym(A);
-        d = A*ones(nv,1);
-        L_tau = d*d';
-    elseif strcmp(method,'degreg')
-        D_tau = mean(A(:));
-        L_tau = diag(sqrt(1./D_tau))*A*diag(sqrt(1./D_tau));
     end
     [U1,~] = eigs(L_tau,K);
     % Row renormalization for spectral clustering
@@ -51,8 +30,7 @@ function [class,U1] = rsc(A, K, method, prior)
     end
     % K-means with restarts
     maxsum = inf;
-    nrestart = 100;
-    for i=1:nrestart,
+    for i=1:10,
         [class0,~,sumD] = kmeans(U1(:,1:K),K);
 	if maxsum > sum(sumD),
 		maxsum = sum(sumD);
